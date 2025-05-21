ec87525894008b2672b60aae784c680134712802
john-kelly <johnkellytemena@gmail.com>
2025-05-21 18:15:55 +0800

    Add hash method

--- modules/hashmap.js
+++ modules/hashmap.js
@@ -4,4 +4,17 @@ export class HashMap {
     this.capacity = 16;
     this.loadfactor = 0.75;
   }
+
+  hash(key) {
+    let hashCode = 0;
+      
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+      hashCode = hashCode % 16;
+    }
+
+    return hashCode;
+  } 
 }
+
