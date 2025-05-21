4ff3126878d3911ac8302d94661536aed3611a8d
john-kelly <johnkellytemena@gmail.com>
2025-05-21 17:21:05 +0800

    Add append method

--- modules/linked-list.js
+++ modules/linked-list.js
@@ -4,4 +4,23 @@ export class LinkedList {
   constructor () {
     this.head = null;
   }
+
+  
+  append(key, value) {
+    let node = new Node();
+    node.key = key;
+    node.value = value;
+
+    if (this.head === null) {
+      this.head = node;
+      return;
+    }
+
+    let curr = this.head;
+    while (curr.next !== null) {
+      curr = curr.next;
+    }
+
+    curr.next = node;
+  }
 }
